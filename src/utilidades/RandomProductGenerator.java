766e074f2652daee4a867ad504490b36370e053a
usr2775 <usr2775@salesianos-lacuesta.net>
2023-05-02 13:09:52 +0100

    feat:(RandomProductGenerator): agregado atributo y un metodo para generar producto aleaotorio

--- src/utilidades/RandomProductGenerator.java
+++ src/utilidades/RandomProductGenerator.java
@@ -5,6 +5,9 @@ public class RandomProductGenerator {
 
     private static final String[] producto = {"Leche", "Huevos", "Queso", "Manzanas", "Pan", "Arroz", "Pollo", "Carne", "Atún", "Aceite", "Pasta", "Coca-cola"};
 
-
+    public static String getRandomProduct() {
+        Random random = new Random();
+        return producto[random.nextInt(producto.length)];
+    }
 
 }
