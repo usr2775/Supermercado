7bb22ae7ab1f78a826926c1fe2e6bb94f7466f37
usr2775 <usr2775@salesianos-lacuesta.net>
2023-05-02 10:27:14 +0100

    feat: (RandomProductGenerator) añadir array estatico de producto

--- src/utilidades/RandomProductGenerator.java
+++ src/utilidades/RandomProductGenerator.java
@@ -1,6 +1,8 @@
 package utilidades;
 
 public class RandomProductGenerator {
-    
+
+    private static final String[] producto = {"Leche", "Huevos", "Queso", "Manzanas", "Pan", "Arroz", "Pollo", "Carne", "Atún", "Aceite", "Pasta", "Coca-cola"};
+
 
 }
