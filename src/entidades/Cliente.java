781718653628442a9e737fe88af592ac074b2734
usr2775 <usr2775@salesianos-lacuesta.net>
2023-05-02 16:39:21 +0100

    feat(cliente): creamos un contructor

--- src/entidades/Cliente.java
+++ src/entidades/Cliente.java
@@ -6,4 +6,11 @@ import java.util.ArrayList;
 public class Cliente {
     private String nombre;
     private ArrayList<String> cesta;
+
+    public Cliente(String nombre, ArrayList<String> cesta) {
+        this.nombre = nombre;
+        cesta = new ArrayList<String>();
+
+
+    }
 }
