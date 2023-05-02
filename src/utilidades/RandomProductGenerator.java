11aff4337a485faa34b7c917acb5cd0f733bb48f
usr2775 <usr2775@salesianos-lacuesta.net>
2023-05-02 10:29:30 +0100

    feat:(RandomProductGenerator): importe Random

--- src/utilidades/RandomProductGenerator.java
+++ src/utilidades/RandomProductGenerator.java
@@ -1,8 +1,10 @@
 package utilidades;
+import java.util.Random;
 
 public class RandomProductGenerator {
 
     private static final String[] producto = {"Leche", "Huevos", "Queso", "Manzanas", "Pan", "Arroz", "Pollo", "Carne", "Atún", "Aceite", "Pasta", "Coca-cola"};
+
 
 
 }
