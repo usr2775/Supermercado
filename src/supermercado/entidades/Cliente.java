bd24bf92164e627c737e87c914a27b2cce878165
usr2775 <usr2775@salesianos-lacuesta.net>
2023-05-02 17:39:40 +0100

    feat(cliente): creado un toString

--- src/supermercado/entidades/Cliente.java
+++ src/supermercado/entidades/Cliente.java
@@ -34,6 +34,21 @@ import java.util.Queue;
             return cesta.size();
         }
 
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Cliente\n");
+            sb.append("===================================\n");
+            sb.append("• Nombre: ").append(nombre).append("\n");
+            sb.append("• Total de productos: ").append(getCantidadProductos()).append("\n");
+            sb.append("• Lista de artículos en la cesta:\n");
+            for (String producto : cesta) {
+                sb.append(producto).append("\n");
+            }
+            sb.append("===================================\n");
+            return sb.toString();
+        }
+
 
 
     }
