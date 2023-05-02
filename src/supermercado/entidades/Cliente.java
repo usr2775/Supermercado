8e502ff29e5e1e91913ef82d1170d45090965052
usr2775 <usr2775@salesianos-lacuesta.net>
2023-05-02 17:03:12 +0100

    feat(cliente): cambiamos los atributos, agregamos nuevas utilidades y constructores

---
+++ src/supermercado/entidades/Cliente.java
@@ -0,0 +1,17 @@
+package supermercado.entidades;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+    public class Cliente {
+        private static int idGlobal = 1;
+        private final int id;
+        private final String nombre;
+        private final Queue<String> cesta;
+
+        public Cliente(String nombre) {
+            this.id = idGlobal++;
+            this.nombre = nombre;
+            this.cesta = new LinkedList<>();
+        }
+}
