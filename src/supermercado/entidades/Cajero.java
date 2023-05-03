a3bdbc1cb556d949ed5a4bc9917580bfcd97f424
usr2775 <usr2775@salesianos-lacuesta.net>
2023-05-03 14:07:45 +0100

    feat(cajero): crearemos los atributos

--- src/supermercado/entidades/Cajero.java
+++ src/supermercado/entidades/Cajero.java
@@ -2,5 +2,10 @@ package supermercado.entidades;
 import java.util.LinkedList;
 import java.util.Queue;
 public class Cajero {
+    private int numeroCaja;
+    private Queue<Cliente> colaClientes;
+
 
 }
+
+
