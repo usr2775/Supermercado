6d3f52f13aa51ac1c2b74acf86cf2f26be8bb529
usr2775 <usr2775@salesianos-lacuesta.net>
2023-05-04 19:43:30 +0100

    fix(Cliente): arreglado del codigo

--- src/supermercado/entidades/Cliente.java
+++ src/supermercado/entidades/Cliente.java
@@ -3,52 +3,49 @@ package supermercado.entidades;
 import java.util.Stack;
 
 
-    public class Cliente {
-        private static int idGlobal = 1;
-        private final int id;
-        private final String nombre;
-        private final Stack<String> cesta;
+public class Cliente {
 
-        public Cliente(String nombre) {
-            this.id = idGlobal++;
-            this.nombre = nombre;
-            this.cesta = new Stack<>();
-        }
+    private String nombre;
+    private int numProductos;
 
-        public int getId() {
-            return id;
-        }
-
-        public String getNombre() {
-            return nombre;
-        }
-        public void agregarProducto(String producto) {
-            cesta.push(producto);
-        }
-
-        public String sacarProducto() {
-            return cesta.pop();
-        }
-
-        public int getCantidadProductos() {
-            return cesta.size();
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            sb.append("Cliente\n");
-            sb.append("===================================\n");
-            sb.append("• Nombre: ").append(nombre).append("\n");
-            sb.append("• Total de productos: ").append(getCantidadProductos()).append("\n");
-            sb.append("• Lista de artículos en la cesta:\n");
-            for (String producto : cesta) {
-                sb.append(producto).append("\n");
-            }
-            sb.append("===================================\n");
-            return sb.toString();
-        }
+    private final Stack<String> cesta;
 
 
-
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+        this.cesta = new Stack<>();
     }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void agregarProducto(String producto) {
+        cesta.push(producto);
+    }
+
+    public String sacarProducto() {
+        return cesta.pop();
+    }
+
+    public int getCantidadProductos() {
+        return cesta.size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente\n");
+        sb.append("===================================\n");
+        sb.append("• Nombre: ").append(nombre).append("\n");
+        sb.append("• Total de productos: ").append(getCantidadProductos()).append("\n");
+        sb.append("• Lista de artículos en la cesta:\n");
+        for (String producto : cesta) {
+            sb.append(producto).append("\n");
+        }
+        sb.append("===================================\n");
+        return sb.toString();
+    }
+
+}
